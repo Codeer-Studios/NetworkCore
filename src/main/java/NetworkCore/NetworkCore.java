f02f88910b13684575818e72796d05228871460b
Thomas Froud <thomasfroud10@gmail.com>
2025-07-24 12:48:11 +0100

    Re-done the idea of Network core

--- src/main/java/NetworkCore/NetworkCore.java
+++ src/main/java/NetworkCore/NetworkCore.java
@@ -1,9 +1,5 @@
 package NetworkCore;
 
-import NetworkCore.Configs.CoreConfig;
-import com.google.gson.Gson;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
@@ -12,90 +8,29 @@ import com.velocitypowered.api.proxy.ProxyServer;
 import javax.inject.Inject;
 import java.util.logging.Logger;
 
-import org.yaml.snakeyaml.Yaml;
-import java.io.File;
-import java.io.InputStream;
-import java.nio.file.Files;
-
 
 @Plugin(id = "NetworkCore", name = "NetworkCore", version = "1.0", authors = {"Thomas Froud"})
-public final class NetworkCore {
+public class NetworkCore {
 
-    public static NetworkCore INSTANCE;
-    public final ProxyServer server;
-    public final Logger logger;
-    public HikariDataSource dataSource;
-    public final Gson gson = new Gson();
-    public CoreConfig config;
+    private final ProxyServer server;
+    private final Logger logger;
 
     @Inject
     public NetworkCore(ProxyServer server, Logger logger) {
         this.server = server;
         this.logger = logger;
-        INSTANCE = this;
     }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
-        logger.info("Initializing NetworkCore...");
-        config = loadConfig();
-        setupDatabase();
-        PluginMessageRouter.init();
-    }
-
-    private void setupDatabase() {
-        CoreConfig.Database db = config.database;
-
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:mysql://" + db.host + ":" + db.port + "/" + db.name + "?useSSL=false");
-        config.setUsername(db.user);
-        config.setPassword(db.password);
-        config.setMaximumPoolSize(db.poolSize);
-        config.setPoolName("NetworkCore-Pool");
-
-        dataSource = new HikariDataSource(config);
-        logger.info("MySQL connection pool initialized.");
-    }
-
-    private CoreConfig loadConfig() {
-        try {
-            File dataFolder = new File("plugins/NetworkCore");
-            if (!dataFolder.exists()) dataFolder.mkdirs();
-
-            File configFile = new File(dataFolder, "config.yml");
-            if (!configFile.exists()) {
-                try (InputStream in = getClass().getResourceAsStream("/config.yml")) {
-                    Files.copy(in, configFile.toPath());
-                    logger.info("Generated default config.yml");
-                }
-            }
-
-            Yaml yaml = new Yaml();
-            try (InputStream in = Files.newInputStream(configFile.toPath())) {
-                return yaml.loadAs(in, CoreConfig.class);
-            }
-
-        } catch (Exception e) {
-            logger.severe("Failed to load config.yml: " + e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static HikariDataSource getDataSource() {
-        return INSTANCE.dataSource;
-    }
-
-    public static Gson getGson() {
-        return INSTANCE.gson;
-    }
-
-    public static ProxyServer getProxy() {
-        return INSTANCE.server;
+        logger.info("[NetworkCore] Initialized and ready for plugin communication.");
     }
 
     public static Logger getLogger() {
-        return INSTANCE.logger;
+        return java.util.logging.Logger.getLogger("NetworkCore");
     }
 
-
+    public ProxyServer getServer() {
+        return server;
+    }
 }
---
+++ src/main/java/NetworkCore/PluginMessageRouter.java
@@ -0,0 +1,25 @@
+package NetworkCore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class PluginMessageRouter {
+
+    private static final Map<String, Consumer<String>> pluginHandlers = new HashMap<>();
+
+    public static void registerPlugin(String channel, Consumer<String> handler) {
+        pluginHandlers.put(channel, handler);
+    }
+
+    public static void sendMessage(String channel, String message) {
+        Consumer<String> handler = pluginHandlers.get(channel);
+        if (handler != null) {
+            handler.accept(message);
+        }
+    }
+
+    public static boolean isPluginRegistered(String channel) {
+        return pluginHandlers.containsKey(channel);
+    }
+}
